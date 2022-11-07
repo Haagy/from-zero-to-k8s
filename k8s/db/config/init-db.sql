c7df020d61f3330d464daa2c0244141a29bb919e
alexander haag <alexander_haag@gmx.de>
2022-11-07 11:52:34 +0100

    Fix init db

--- k8s/db/config/init-db.sql
+++ k8s/db/config/init-db.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE rest-api-db;
-CREATE USER rest-api-user WITH ENCRYPTED PASSWORD 'yourpass';
+CREATE USER rest-api-user WITH ENCRYPTED PASSWORD 'postgres';
 GRANT ALL PRIVILEGES ON DATABASE rest-api-db TO rest-api-user;
